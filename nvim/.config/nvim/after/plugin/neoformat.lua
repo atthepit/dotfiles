a548343756672e09ae456354755b9e1282c97d82
Pedro P <gitpit.latonia@8alias.com>
2023-08-25 19:07:59 +0200

    Remove configuration and import Kickstart.nvim

--- nvim/.config/nvim/after/plugin/neoformat.lua
+++
@@ -1,5 +0,0 @@
--- [[ Configure Neoformat ]]
--- See `:help neoformat`
--- vim.g.neoformat_try_node_exe = 1
--- vim.cmd('autocmd BufWritePre *.{js,jsx} Neoformat')
-
--- nvim/.config/nvim/lua/thepit/init.lua
+++
@@ -1,3 +0,0 @@
-require("thepit.remap")
-require("thepit.set")
-
