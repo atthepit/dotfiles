86fe40b27b4ae0d34a7ad16879b4eabde750a05c
Pedro P <gitpit.latonia@8alias.com>
2023-08-25 18:54:30 +0200

    Add some comments to remaps

--- nvim/.config/nvim/lua/thepit/remap.lua
+++ nvim/.config/nvim/lua/thepit/remap.lua
@@ -24,19 +24,17 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Removes whitespaces down
 vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP") -- Paste without replacing yanked
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- Yank to clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]]) -- Yank line to clipboard
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) -- drop without replacing yank
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>") -- Ctrl-c to esc
 
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- Set current file as executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
